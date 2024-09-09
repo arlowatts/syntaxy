a11f9d5fd2c906cc64b919a8e574c23b7b115505
Arlo Watts <arlowatts1@gmail.com>
2024-09-09 10:10:32 -0700

    Add javadoc comments for Identifier

--- src/main/java/Identifier.java
+++ src/main/java/Identifier.java
@@ -1,11 +1,23 @@
+/**
+ * An `Identifier` matches a `String` if its referenced `Expression` matches that `String`.
+ * Careless use of `Identifier`s can lead to infinite recursion when matching or generating a `String` if there exists a closed loop, such as an `Identifier` referencing itself.
+ **/
 public class Identifier implements Expression {
     private String value;
     private Expression expression;
 
+    /**
+     * Initializes a newly created `Identifier` with the given name and a `null` `Expression`.
+     * @param value a `String`.
+     **/
     public Identifier(String value) {
         this.value = value;
     }
 
+    /**
+     * Sets this `Identifier`'s reference to an `Expression`.
+     * @param expression the `Expression` this `Identifier` will reference.
+     **/
     public void setExpression(Expression expression) {
         this.expression = expression;
     }
